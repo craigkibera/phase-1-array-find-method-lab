f19692883dd29d4ba7c935833f617d6c2dabf6f6
craigkibera <craigituro@gmail.com>
2024-09-30 11:56:31 +0300

    method array

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(array) {
+    let result = array.find(obj => obj.result === "W")
+    if(result) {
+        return result.year
+    } else return undefined
+}
+superbowlWin(record)
